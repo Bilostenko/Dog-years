e062cd74d40c475c7fc6180adb1c40997c4bb0f4
Bilostenko <bilostenkod@gmail.com>
2021-11-06 13:03:12 +0200

    Dog_age_calculator_final

---
+++ main.js
@@ -0,0 +1,8 @@
+let myAge = 31; /*  my human age */
+let earlyYears = 2; /* dog value */
+earlyYears = earlyYears * 10.5;
+let laterYears = myAge -2; /*number of dog years accounted for by your later years */
+laterYears *=4 ;/*  dog years */
+myAgeInDogYears = earlyYears + laterYears; /* my age in dog age */
+myName = "Dima".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`) /* statement */
